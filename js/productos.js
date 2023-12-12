7afdbf4508ec4ccaebc92c5cc49bf3856d834452
aaaalleexx11 <20151606@aguascalientes.tecnm.mx>
2023-12-12 10:44:28 -0800

    commit1

---
+++ js/productos.js
@@ -0,0 +1,102 @@
+let productos = [
+    {
+        id: 1234234212,
+        nombre: "LED Foco Philips LEDH8H11H16",
+        descripcion: "Foco LED de alta calidad de la marca Philips, ofrece iluminación eficiente y duradera para tu vehículo.",
+        descripcion_corta: "Foco LED de Philips para iluminación vehicular.",
+        categoria: "Accesorios para Automóviles",
+        precio: 150.00,
+        stock: 100,
+        imagen: "img/img1.png"
+    },
+    {
+        id: 267678678856,
+        nombre: "Balatas de Ceramica Duralast MKD600",
+        descripcion: "Balatas de cerámica Duralast MKD600 para un rendimiento de frenado confiable y duradero.",
+        descripcion_corta: "Balatas de cerámica Duralast MKD600 para frenos.",
+        categoria: "Accesorios para Automóviles",
+        precio: 65.00,
+        stock: 220,
+        imagen: "img/img2.png"
+    },
+    {
+        id: 37896342452,
+        nombre: "Rotor de Freno Duralast C-DL31340",
+        descripcion: "Rotor de freno Duralast C-DL31340 para un frenado óptimo y seguro en tu vehículo.",
+        descripcion_corta: "Rotor de freno Duralast C-DL31340.",
+        categoria: "Accesorios para Automóviles",
+        precio: 90.00,
+        stock: 450,
+        imagen: "img/img3.png"
+    },
+    {
+        id: 401241219248,
+        nombre: "Bujia de Iridio Autolite Ultra AI3922",
+        descripcion: "Bujía de iridio Autolite Ultra AI3922 para un encendido eficiente y potente del motor.",
+        descripcion_corta: "Bujía de iridio Autolite Ultra AI3922.",
+        categoria: "Accesorios para Automóviles",
+        precio: 25.00,
+        stock: 82,
+        imagen: "img/img4.png"
+    },
+    {
+        id: 5234132423498,
+        nombre: "Alternador Duralast DL13380-7S",
+        descripcion: "Alternador Duralast DL13380-7S para generar energía confiable en tu vehículo.",
+        descripcion_corta: "Alternador Duralast DL13380-7S.",
+        categoria: "Accesorios para Automóviles",
+        precio: 30.00,
+        stock: 25,
+        imagen: "img/img5.png"
+    },
+    {
+        id: 6023482342381,
+        nombre: "STP Filtro de Aceite SL8712",
+        descripcion: "Filtro de aceite STP SL8712 para mantener el motor limpio y protegido.",
+        descripcion_corta: "Filtro de aceite STP SL8712.",
+        categoria: "Accesorios para Automóviles",
+        precio: 65.00,
+        stock: 810,
+        imagen: "img/img6.png"
+    },
+    {
+        id: 71121235845,
+        nombre: "Duralast Loaded Ensamble - Amortiguador LS34-85491B",
+        descripcion: "Ensamble de amortiguador Duralast Loaded LS34-85491B para una conducción suave y segura.",
+        descripcion_corta: "Ensamble de amortiguador Duralast Loaded LS34-85491B.",
+        categoria: "Accesorios para Automóviles",
+        precio: 85.00,
+        stock: 30,
+        imagen: "img/img7.png"
+    },
+    {
+        id: 9232734827347,
+        nombre: "Duralast Banda 660K6",
+        descripcion: "Banda Duralast 660K6 para un funcionamiento eficiente del motor.",
+        descripcion_corta: "Banda Duralast 660K6.",
+        categoria: "Accesorios para Automóviles",
+        precio: 130.00,
+        stock: 78,
+        imagen: "img/img8.png"
+    },
+    {
+        id: 9234823423,
+        nombre: "Duralast Bobina de Encendido C1149",
+        descripcion: "Bobina de encendido Duralast C1149 para un encendido confiable del motor.",
+        descripcion_corta: "Bobina de encendido Duralast C1149.",
+        categoria: "Accesorios para Automóviles",
+        precio: 220.00,
+        stock: 24,
+        imagen: "img/img9.png"
+    },
+    {
+        id: 10324234234,
+        nombre: "Duralast Terminal de Bateria Poste Superior DL00316",
+        descripcion: "Terminal de batería Duralast DL00316 para una conexión segura y confiable.",
+        descripcion_corta: "Terminal de batería Duralast DL00316.",
+        categoria: "Accesorios para Automóviles",
+        precio: 190.00,
+        stock: 600,
+        imagen: "img/img10.png"
+    },
+];
